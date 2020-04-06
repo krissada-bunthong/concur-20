71db50f82b89b26eaa3fee548d301d12785282e3
Supawit W <wattanapairotrats@gmail.com>
2020-04-06 17:20:23 +0700

    update2: create book transaction function

--- alpha/CC20_0.0.1a.py
+++ alpha/CC20_0.0.1a.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify
-import random, math
+import random, math, time
 
 app = Flask(__name__)
 
@@ -10,29 +10,51 @@ def hotel_api(ratio=(0.5,0.5),delay=0):
     li_zero = [0 for i in range(math.ceil(ratio[0])+1)]
     li_one.extend(li_zero)
     ans = random.choice(li_one)
+    if delay > 0:
+        time.sleep(delay)
+        
     if ans is 1:
         return True
     else:
         return False
 
 # Flight API for Request and Book
-def flight_api(ratio=(0.5,0.5),delay=0)):
+def flight_api(ratio=(0.5,0.5),delay=0):
     li_one = [1 for i in range(math.ceil(ratio[1])+1)]
     li_zero = [0 for i in range(math.ceil(ratio[0])+1)]
     li_one.extend(li_zero)
     ans = random.choice(li_one)
+    if delay > 0:
+        time.sleep(delay)
+
     if ans is 1:
         return True
     else:
         return False
 
+from concurrent.futures import ThreadPoolExecutor
+
 # Thread process for booking trip
 def book():
-    pass
+    with ThreadPoolExecutor(max_workers=2) as exe:
+        resp_h = exe.submit(hotel_api)
+        resp_f = exe.submit(flight_api)
+        if resp_h and resp_f:
+            return True
+        else:
+            return {"hotel_resp":resp_h,
+                    "flight_resp":resp_f}
 
 # Thread process for request api data
 def api_call():
-    pass
+    with ThreadPoolExecutor(max_workers=2) as exe:
+        resp_h = exe.submit(hotel_api)
+        resp_f = exe.submit(flight_api)
+        if resp_h and resp_f:
+            return True
+        else:
+            return {"hotel_resp":resp_h,
+                    "flight_resp":resp_f}
 
 
 # Index Page
@@ -43,8 +65,16 @@ def index():
 # Book Page
 @app.route('/book', methods=['GET'])
 def get_book():
-    pass
+    resp_chk = api_call()
+    if resp_chk:
+        resp_book = book()
+    else:
+        return resp_chk
 
+# create member data
+@app.route('/member', methods=['POST'])
+def create_member():
+    pass
 
 if __name__ == "__main__":
 	app.run(debug=True)
